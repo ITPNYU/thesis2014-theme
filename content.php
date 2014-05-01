74495890f6685410524f538b2a2760e30a839de6
Karl Ward <karl@ghostghost.net>
2014-05-01 16:00:51 -0400

    removed some extraneous navigation --KW

--- content.php
+++ content.php
@@ -46,12 +46,6 @@
 	<div class="entry-content">
 		<?php
 			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-			) );
 		?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
