982cdebe898dc7d493fd78a565e9afb657631683
Karl Ward <karl@ghostghost.net>
2014-05-02 11:38:29 -0400

    modified content-listview --KW

--- content-listview.php
+++ content-listview.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * The default template for displaying content
- *
- * Used for both single and index/archive/search.
+ * The template for displaying a content item in a list
  *
  * @package WordPress
  * @subpackage Twenty_Fourteen
@@ -10,35 +8,17 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php twentyfourteen_post_thumbnail(); ?>
 
 	<header class="entry-header">
 		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
 		<?php
 			endif;
-
-			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-			echo "<em>" . get_post_meta( get_the_ID(), 'student', true) . "</em>\n";
+			echo "<li>";
+			the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' );
+			echo "- <em>" . get_post_meta( get_the_ID(), 'student', true) . "</em></li>\n";
 		?>
 
-		<div class="entry-meta">
-			<?php
-				if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
-			?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
-			<?php
-				endif;
-
-				edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
-			?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
-	<div class="entry-summary">
-	<?php
-	echo "<p>" . get_post_meta( get_the_ID(), 'elevator_pitch', true) . "</p>\n";
-	?>
-	</div><!-- .entry-summary -->
-
 	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
 </article><!-- #post-## -->
