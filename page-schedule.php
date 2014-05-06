7a1b99d597ec8c21f600c23d5b2c7192246b8dfb
Karl Ward <karl@ghostghost.net>
2014-05-06 11:06:21 -0400

    added template name to file --KW

--- page-schedule.php
+++ page-schedule.php
@@ -1,14 +1,7 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other 'pages' on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+ * Template Name: Schedule
+ * The template for displaying the wide schedule page
  */
 
 get_header(); ?>
